ec67c1a9a642446886e25c8da7912f733176ff66
Saoud2021 <abdallahnasser2015@gmail.com>
2024-12-21 20:07:52 +0200

    change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zougari47.github.io',
-  base: '/portfolio-template',
+  site: 'https://saoud2021.github.io',
+  base: '/Saoud2021',
   integrations: [tailwind()],
 })
